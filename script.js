247dda6fb572e2d4d23f5f80938317195a845de2
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-05-24 13:40:07 +0100

    creates hover effect

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+const container = document.querySelector('.container');
 //creates cells of grid
 function createGrid(cell) {
-      const container = document.querySelector('.container');
-      for (let i = 0; i < cell; i++) {
-        for (let j = 0; j < cell; j++) {
-          let grid = document.createElement('div')
-          grid.className = 'cells';
-          container.append(grid);
-          container.style.setProperty('grid-template-columns', 'repeat(' + cell + ', 1fr)' );
-        }
-      }
+  for (let i = 0; i < cell; i++) {
+    for (let j = 0; j < cell; j++) {
+      const grid = document.createElement('div')
+      grid.className = 'cells';
+      container.append(grid);
+      container.style.setProperty('grid-template-columns', 'repeat(' + cell + ', 1fr)' );
     }
+  }
+}
 
-    createGrid(20);
+createGrid(20);
+
+//creates hover effect
+function hover() {
+  container.addEventListener('mouseover', (e) => {
+    let grid = e.target;
+    grid.style.setProperty('background', 'blue')
+  });
+}
+
+hover();
