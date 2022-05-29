1612b6735ea62eded067cc5bf146c1fd72b1a0aa
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-05-29 18:28:23 +0100

    create grid based on user input

--- script.js
+++ script.js
@@ -6,19 +6,37 @@ function createGrid(cell) {
       const grid = document.createElement('div')
       grid.className = 'cells';
       container.append(grid);
-      container.style.setProperty('grid-template-columns', 'repeat(' + cell + ', 1fr)' );
+      container.style.setProperty('grid-template-columns', `repeat(${cell}, 1fr`);
     }
   }
 }
-
-createGrid(20);
 
 //creates hover effect
 function hover() {
   container.addEventListener('mouseover', (e) => {
     let grid = e.target;
-    grid.style.setProperty('background', 'blue')
+    grid.style.setProperty('background', 'black')
   });
 }
 
-hover();
+//creates grid based on user input
+function buttonClick() {
+  document.querySelector('.btn').addEventListener
+  ('click', () => {
+    const num = parseInt(prompt("How many rows and columns would like ?"));
+    if (num <= 100) {
+      clearGrid();
+      createGrid(num);
+      hover();
+    } else {
+      alert("Please input numbers between 1- 100")
+    }
+  });
+}
+
+buttonClick();
+
+//clear grid
+function clearGrid() {
+  container.innerHTML = ''
+}
