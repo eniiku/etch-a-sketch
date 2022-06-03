ae54afd122caa4dace28ba75fdad28fc162bb30f
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-06-03 04:43:59 +0100

    script <unknwon>

--- script.js
+++ script.js
@@ -11,12 +11,28 @@ function createGrid(cell) {
   }
 }
 
-//creates hover effect
-function hover() {
+//selects color based on input
+function setColor() {
+  const random = document.querySelector('.random-btn');
+  const black = document.querySelector('.black-btn');
+
   container.addEventListener('mouseover', (e) => {
-    let grid = e.target;
-    grid.style.setProperty('background', 'black')
+    let settings = e.target;
+    if (random == true) {
+      random.addEventListener('click', () => {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        settings.style.background = `rgb(${randomR}, ${randomG}, ${randomB})`
+      });
+    } else if (black == true) {
+      settings.style.backgroundColor = '#000'
+    }
   });
+  // container.addEventListener('mouseover', (e) => {
+  //   let grid = e.target;
+  //   grid.style.setProperty('background', 'black')
+  // });
 }
 
 //creates grid based on user input
@@ -27,7 +43,7 @@ function buttonClick() {
     if (num <= 100) {
       clearGrid();
       createGrid(num);
-      hover();
+      setColor();
     } else {
       alert("Please input numbers between 1- 100")
     }
